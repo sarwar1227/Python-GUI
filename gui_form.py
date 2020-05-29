64dd70043682d7f4547fb3c8c2b4a73efca6abc9
Sarwar Ali <47899529+sarwar1227@users.noreply.github.com>
2020-05-29 11:30:01 +0530

    Radio Buttons added for gender selection

--- gui_form.py
+++ gui_form.py
@@ -22,20 +22,21 @@ def sarwar(event):
         food = "Yes"
     else:
         food = "No"
+    gender_dic={ 1:"Male",2:"Female",3:"Others"}
     ans=tmsg.askquestion("Confirm Booking","Do You Really Want to Book Your Ticket?")
     if ans=="yes":
         with open("record.txt","a") as f:
-            f.write(f"{namevalue.get()} {phonevalue.get()} {gendervalue.get()} {emergencyvalue.get()} {paymentmodevalue.get()} {food}\n")
+            f.write(f"{namevalue.get()} {phonevalue.get()} {gender_dic[gendervalue.get()]} {emergencyvalue.get()} {paymentmodevalue.get()} {food}\n")
             ans2=tmsg.showinfo("Confirmation Popup", "Booking Successfull !!")
             if ans2=="ok":
                 israr.set("Your Ticket is Booked !!")
-                Label(root, textvariable=israr).grid(row=8, column=3)
+                Label(root, textvariable=israr).grid(row=11, column=3)
                 button=Button(root,text="Book Again")
-                button.grid(row=9,column=3)
+                button.grid(row=12,column=3)
                 button.bind('<Button-1>',reset_values)
                 Label(root, text="Please Rate Our App").grid(row=13,column=3)
-                slider.grid(row=12,column=3)
-                Button(root, text="Rate",command=rate).grid(row=14,column=3)
+                slider.grid(row=14,column=3)
+                Button(root, text="Rate",command=rate).grid(row=15,column=3)
 Label(root,text="Welcome to sarwar travels",bg="pink",fg="black",font="comicsansms 15 bold",).grid(row=0,column=3)
 name=Label(root,text="Name",fg="black",bg="white")
 phone=Label(root,text="Phone",fg="black",bg="white")
@@ -46,26 +47,30 @@ paymentmode=Label(root,text="Payment Mode",fg="black",bg="white")
 name.grid(row=1,column=2)
 phone.grid(row=2,column=2)
 gender.grid(row=3,column=2)
-emergency.grid(row=4,column=2)
-paymentmode.grid(row=5,column=2)
+emergency.grid(row=7,column=2)
+paymentmode.grid(row=8,column=2)
 
 namevalue=StringVar()
 phonevalue=StringVar()
-gendervalue=StringVar()
+gendervalue=IntVar()
 emergencyvalue=StringVar()
 paymentmodevalue=StringVar()
 foodservicevalue=IntVar()
 
+gender_list=["Male","Female","Others"]
+value_list=[1,2,3]
+
 Entry(root,textvariable=namevalue).grid(row=1,column=3)
 Entry(root,textvariable=phonevalue).grid(row=2,column=3)
-Entry(root,textvariable=gendervalue).grid(row=3,column=3)
-Entry(root,textvariable=emergencyvalue).grid(row=4,column=3)
+for i in range(0,3):
+    radio=Radiobutton(root,text=gender_list[i],value=value_list[i],variable=gendervalue).grid(row=i+3,column=3)
+Entry(root,textvariable=emergencyvalue).grid(row=7,column=3)
 entry=Entry(root,textvariable=paymentmodevalue)
-entry.grid(row=5,column=3)
+entry.grid(row=8,column=3)
 check=Checkbutton(root,text="Want to prebook your meal ?",variable=foodservicevalue,font="arial 10 bold")
-check.grid(row=6,column=3)
+check.grid(row=9,column=3)
 button=Button(root,text="Book")
-button.grid(row=7,column=3)
+button.grid(row=10,column=3)
 button.bind('<Button-1>',sarwar)
 check.bind('<Return>',sarwar)
 entry.bind('<Return>',sarwar)
